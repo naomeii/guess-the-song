d2cd17121766eecc225c3485b056d1ec2fc1b741
naomeii <1naomiari@gmail.com>
2024-08-23 11:36:10 -0400

    added port infos

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 10000, // the port Render expects
+    strictPort: true,
+  },
+  preview: {
+    port: 10000, // same port for npm run preview
+  },
 })
